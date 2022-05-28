00c712cde7b5874c5c6033354d756717ff4e7645
blelump <blelump@martex.nom.pl>
2022-05-29 00:10:04 +0200

    Bootstrap identifer with keri.js

--- controller1_terminal_app/index.js
+++ controller1_terminal_app/index.js
@@ -1,19 +1,24 @@
 const qrcode = require('qrcode-terminal');
 const inquirer = require("inquirer");
+const nacl = require("tweetnacl");
 
+const { Controller, ConfigBuilder, KeyType, PublicKey, SignatureBuilder } = require("keri.js")
+
+console.log(require("keri.js"));
 const menu = [
   'Perform introduction (OOBI via QR code)',
   'Issue ACDC',
 ];
+if(!process.env.HOST_IP_ADDR) {
+  throw new Error("HOST_IP_ADDR env var must be set!")
+}
+
 const witness1Eid = "BSuhyBcPZEZLK-fcw5tzHn2N46wRCG_ZOoeKtWTOunRA";
+const witness1OOBI = `{"eid":"${witness1Eid}","scheme":"http","url":"http://${process.env.HOST_IP_ADDR}:3232/"}`;
 const witness2Eid = "BVcuJOOJF1IE8svqEtrSuyQjGTd2HhfAkt9y2QkUtFJI";
 const cid = "EFxHNNoySeNhPSv5TUWxY3QIzy_XT9pKI1YLHv355nuY";
 
 (async function() {
-  if(!process.env.HOST_IP_ADDR) {
-    throw new Error("HOST_IP_ADDR env var must be set!")
-  }
-
   let answers = await inquirer.prompt([
     {
       type: 'list',
@@ -26,10 +31,27 @@ const cid = "EFxHNNoySeNhPSv5TUWxY3QIzy_XT9pKI1YLHv355nuY";
   if(answers.op === menu[0]) {
     qrcode.generate(`[
     {"cid":"${cid}","role":"witness","eid":"${witness1Eid}"},
-      {"eid":"${witness1Eid}","scheme":"http","url":"http://${process.env.HOST_IP_ADDR}:3232/"},
+      ${witness1OOBI},
       {"eid":"${witness2Eid}","scheme":"http","url":"http://${process.env.HOST_IP_ADDR}:3233/"}]`, { small: true });
   } else if(answers.op === menu[1]) {
+    let config = new ConfigBuilder().withDbPath("./database")
+      .build();
+    console.log(config);
+    let controller = new Controller(config);
+    let currentKeyPair = nacl.sign.keyPair();
+    let nextKeyPair = nacl.sign.keyPair();
 
+    console.log(
+
+      [ (new PublicKey(KeyType.Ed25519, Buffer.from(currentKeyPair.publicKey))).getKey() ],
+    );
+    let inceptionEvent = controller.incept(
+      [ (new PublicKey(KeyType.Ed25519, Buffer.from(currentKeyPair.publicKey))).getKey() ],
+      [ (new PublicKey(KeyType.Ed25519, Buffer.from(nextKeyPair.publicKey))).getKey() ],
+      [witness1OOBI],
+      1
+    );
+    let key_type = KeyType.Ed25519;
   } else {
     throw new Error('Unsupported operation');
   }
